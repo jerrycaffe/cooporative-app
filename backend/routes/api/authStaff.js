fdf3b0e698d43fd33e0868f1116e29c685fa0e4a
Jerrycaffe <jerrycaffe@gmail.com>
2020-07-13 12:36:48 +0100

    Just a formatting

--- backend/routes/api/authStaff.js
+++ backend/routes/api/authStaff.js
@@ -1,10 +1,10 @@
-import {Router} from 'express';
-import {addStaff, staffLogin} from '../../controllers/authStaffController';
-import {adminProtect} from '../../middleware/auth'
+import { Router } from "express";
+import { addStaff, staffLogin } from "../../controllers/authStaffController";
+import { adminProtect } from "../../middleware/auth";
 
 const router = Router();
 
-router.post('/add', adminProtect, addStaff);
-router.post('/login', staffLogin);
+router.post("/add", adminProtect, addStaff);
+router.post("/login", staffLogin);
 
 export default router;
